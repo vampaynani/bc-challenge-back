7174a9e5170b251141df4104e40991cc8a4527d5
Wenceslao Negrete <vampay@digitaldealers.mx>
2023-06-12 10:58:25 -0600

    Backend MVP

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 require("dotenv").config();
 
-async function main() {}
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-main();
+const app = express();
+
+const history = [];
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/history", (req, res) => {
+  history.push(req.body);
+  res.json({ success: true });
+});
+
+app.get("/history", (req, res) => {
+  res.json(history);
+});
+
+app.listen(8000, () => {
+  console.log("server is running");
+});
